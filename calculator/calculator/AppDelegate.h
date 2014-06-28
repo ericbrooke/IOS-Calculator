711d8723a49e5bc957f7c7f167faadfebb257b4f
Eric Brooke <ericbrooke@mac.com>
2014-06-28 12:32:11 -0700

    Basic setup

---
+++ calculator/calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by Eric Brooke on 2014-06-28.
+//  Copyright (c) 2014 Eric Brooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calculator/calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Eric Brooke on 2014-06-28.
+//  Copyright (c) 2014 Eric Brooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
